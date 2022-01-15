6541e6910d8532d943d799ae18573a1c9e61cd52
Wachiputi <bsc-com-35-19@unima.ac.mw>
2022-01-15 18:29:12 +0200

    added java comments

--- khomosystem/src/main/java/com/tdcodes/khomosystem/KhomosystemApplication.java
+++ khomosystem/src/main/java/com/tdcodes/khomosystem/KhomosystemApplication.java
@@ -1,12 +1,16 @@
+//a package calle khomosystem
 package com.tdcodes.khomosystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//creates a class
 public class KhomosystemApplication {
 
+	//class's main method
 	public static void main(String[] args) {
+		//access a methos run
 		SpringApplication.run(KhomosystemApplication.class, args);
 	}
 
